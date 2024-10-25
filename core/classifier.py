9514024fcd3589f816526c773dfe71b30c7b0d61
gzordrai <40698142+gzordrai@users.noreply.github.com>
2024-10-25 17:55:07 +0200

    Implement KNN classifier

--- core/classifier.py
+++ core/classifier.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from core.data_distance import DistanceStrategy, WordOverlapDistance
+from data import Data
+from data_distance import DistanceStrategy, WordOverlapDistance
 
 class Classifier(ABC):
     def __init__(self, strategy: DistanceStrategy = WordOverlapDistance) -> None:
@@ -9,14 +10,23 @@ class Classifier(ABC):
         self.__distance_strategy = strategy
     
     @abstractmethod
-    def classify(self, data) -> str:
+    def classify(self, data: Data, dataset: list[Data] = []) -> int:
         pass
 
 class KNNClassifier(Classifier):
-    def __init__(self, k = 3):
+    def __init__(self, k = 10) -> None:
+        super().__init__()
         self.__k = k
 
-    def classify(self, data) -> str:
-        pass
+    def classify(self, data: Data, dataset: list[Data] = []) -> int:
+        neighbors: list[Data] = dataset[:self.__k]
+        distances: list[int] = [data.distance(neighbor) for neighbor in neighbors]
 
-    
+        for d in dataset[self.__k:]:
+            distance = data.distance(d)
+
+            if distance < max(distances):
+                neighbors[distances.index(max(distances))] = d
+                distances[distances.index(max(distances))] = distance
+
+        return max(set(neighbors), key = neighbors.count).get_annotation()
