f6a5494bda129e6ec87bad0345930b5fe8632af9
gzordrai <40698142+gzordrai@users.noreply.github.com>
2024-10-25 17:58:41 +0200

    Update main to display accurency

--- core/main.py
+++ core/main.py
@@ -1,9 +1,12 @@
-from dataset import Dataset
+from dataset import AnnotatedDataset
 from classifier import Classifier, KNNClassifier
 from os import getcwd
 from os.path import join
 
 if __name__ == "__main__":
-    path: str = join(getcwd(), "datasets/inputs/training.1600000.processed.noemoticon.csv")
+    path: str = join(getcwd(), "datasets/inputs/testdata.manual.2009.06.14.csv")
     classifier: Classifier = KNNClassifier(10)
-    dataset: Dataset = Dataset(path, classifier)
+    accuracy: float = 0.0
+
+    dataset: AnnotatedDataset = AnnotatedDataset(path, classifier)
+    print(f"Accurency {dataset.classify()}%")
