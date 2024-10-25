218a0a6ef07f4cc3f4319e346afbd41338f01c82
gzordrai <40698142+gzordrai@users.noreply.github.com>
2024-10-25 17:54:12 +0200

    Add annotation to data

--- core/data.py
+++ core/data.py
@@ -1,10 +1,11 @@
 from re import sub
-from core.data_distance import DistanceStrategy, WordOverlapDistance
+from data_distance import DistanceStrategy, WordOverlapDistance
 
 class Data():
-    def __init__(self, data: str, distance: DistanceStrategy = WordOverlapDistance) -> None:
+    def __init__(self, annotation: str, data: str, distance: DistanceStrategy = WordOverlapDistance) -> None:
+        self.__annotation = annotation
         self.__data: str = data
-        self.__distance: DistanceStrategy = distance(data)
+        self.__distance: DistanceStrategy = distance()
 
     def clean(self):
         patterns = [
@@ -13,6 +14,7 @@ class Data():
             (r"RT", ""),                                                                                    # Remove retweets
             (r".+ - http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", ""), # Remove attached links
             (r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", ""),      # Remove links
+            (r"\[.*?\]", ""),                                                                               # Remove square brackets
             (r".*[:;][\)][^\n]*[:;][\(].*|.*[:;][\(][^\n]*[:;][\)].*", ""),                                 # Remove happy and sad emoticons in the same tweet
             (r"(?<=[a-zA-Z])[!\?\"\.;,]", r" \g<0>"),                                                       # Add space before punctuation only if there's a letter before
             (r"[!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]", ""),                                                 # Remove punctuation
@@ -26,7 +28,10 @@ class Data():
 
             self.__data = sub(pattern[0], pattern[1], self.__data)
 
-        return Data(self.__data.lower())
+        return Data(self.__annotation, self.__data.lower().strip())
+
+    def get_annotation(self) -> str:
+        return self.__annotation
 
     def get_data(self) -> str:
         return self.__data
