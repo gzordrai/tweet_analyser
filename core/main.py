4d4e52844c7bbacf14eb21a476c86ac07be997ea
gzordrai <40698142+gzordrai@users.noreply.github.com>
2024-11-04 14:33:29 +0100

    Update main file

--- core/main.py
+++ core/main.py
@@ -1,12 +1,29 @@
-from dataset import AnnotatedDataset
+from dataset import AnnotatedDataset, UnannotateDataset
 from classifier import Classifier, KNNClassifier
 from os import getcwd
 from os.path import join
+from tqdm import tqdm
 
 if __name__ == "__main__":
-    path: str = join(getcwd(), "datasets/inputs/testdata.manual.2009.06.14.csv")
+    # annotated_dataset_path: str = join(getcwd(), "datasets/inputs/testdata.manual.2009.06.14.csv")
+    annotated_dataset_path: str = join(getcwd(), "annotated.csv")
+    unannotated_dataset_path: str = join(getcwd(), "datasets/inputs/training.1600000.processed.noemoticon.csv")
     classifier: Classifier = KNNClassifier(10)
     accuracy: float = 0.0
 
-    dataset: AnnotatedDataset = AnnotatedDataset(path, classifier)
-    print(f"Accurency {dataset.classify()}%")
+    annotated_dataset: AnnotatedDataset = AnnotatedDataset(annotated_dataset_path, classifier)
+
+    print(f"Accurency {annotated_dataset.classify()}%")
+
+    #unannotated_dataset: UnannotateDataset = UnannotateDataset(unannotated_dataset_path, classifier, annotated_dataset)
+
+    # unannotated_dataset.classify()
+    # unannotated_dataset.save()
+
+    # k: int = 100
+
+    # for _ in tqdm(range(k)):
+    #     dataset = AnnotatedDataset(annotated_dataset_path, classifier)
+    #     accuracy += dataset.classify()
+
+    # print(f"Accurency {accuracy / k}%")
