72e1f49221f87fbe622f452ea428329c07591224
gzordrai <gzordrai@users.noreply.github.com>
2024-11-18 11:08:31 +0100

    Add Naive Bayes classifier implementation and update main execution flow

--- core/classifier.py
+++ core/classifier.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
+from numpy import log
 from data import Data
 
 class Classifier(ABC):
@@ -27,3 +29,47 @@ class KNNClassifier(Classifier):
                 annotation_counts[annotation] = 1
 
         return max(annotation_counts, key = annotation_counts.get)
+
+class NaiveBayesClassifier(Classifier):
+    def __init__(self) -> None:
+        super().__init__()
+        self.class_probabilities = defaultdict(float)
+        self.feature_probabilities = defaultdict(lambda: defaultdict(float))
+        self.classes = set()
+
+    def train(self, dataset: list[Data]) -> None:
+        class_counts = defaultdict(int)
+        feature_counts = defaultdict(lambda: defaultdict(int))
+        total_count = len(dataset)
+
+        for data in dataset:
+            annotation = data.get_annotation()
+            self.classes.add(annotation)
+            class_counts[annotation] += 1
+
+            words = data.get_data().split()
+
+            for word in words:
+                feature_counts[annotation][word] += 1
+
+        for annotation in self.classes:
+            self.class_probabilities[annotation] = class_counts[annotation] / total_count
+            total_words = sum(feature_counts[annotation].values())
+
+            for word in feature_counts[annotation]:
+                self.feature_probabilities[annotation][word] = (feature_counts[annotation][word] + 1) / (total_words + len(feature_counts[annotation]))
+
+    def classify(self, data: Data, dataset: list[Data] = []) -> int:
+        words = data.get_data().split()
+        class_scores = defaultdict(float)
+
+        for annotation in self.classes:
+            class_scores[annotation] = log(self.class_probabilities[annotation])
+
+            for word in words:
+                if word in self.feature_probabilities[annotation]:
+                    class_scores[annotation] += log(self.feature_probabilities[annotation][word])
+                else:
+                    class_scores[annotation] += log(1 / (sum(self.feature_probabilities[annotation].values()) + len(self.feature_probabilities[annotation])))
+
+        return max(class_scores, key=class_scores.get)
--- core/main.py
+++ core/main.py
@@ -1,19 +1,19 @@
 from dataset import AnnotatedDataset, UnannotateDataset
-from classifier import Classifier, KNNClassifier
+from classifier import Classifier, KNNClassifier, NaiveBayesClassifier
 from os import getcwd
 from os.path import join
 from tqdm import tqdm
 
 if __name__ == "__main__":
     # annotated_dataset_path: str = join(getcwd(), "datasets/inputs/testdata.manual.2009.06.14.csv")
-    annotated_dataset_path: str = join(getcwd(), "annotated.csv")
+    annotated_dataset_path: str = join(getcwd(), "datasets/inputs/testdata.manual.2009.06.14.csv")
     unannotated_dataset_path: str = join(getcwd(), "datasets/inputs/training.1600000.processed.noemoticon.csv")
-    classifier: Classifier = KNNClassifier(10)
+    classifier: Classifier = NaiveBayesClassifier()# KNNClassifier(10)
     accuracy: float = 0.0
 
     annotated_dataset: AnnotatedDataset = AnnotatedDataset(annotated_dataset_path, classifier)
 
-    print(f"Accurency {annotated_dataset.classify()}%")
+    # print(f"Accurency {annotated_dataset.classify()}%")
 
     #unannotated_dataset: UnannotateDataset = UnannotateDataset(unannotated_dataset_path, classifier, annotated_dataset)
 
@@ -27,3 +27,7 @@ if __name__ == "__main__":
     #     accuracy += dataset.classify()
 
     # print(f"Accurency {accuracy / k}%")
+
+    classifier.train(annotated_dataset.get_data())
+    accuracy = annotated_dataset.classify()
+    print(f"Accuracy: {accuracy}%")
